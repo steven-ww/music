1e22cc65bed4acea73f17fb98e7c9dd1f26ce31e
Steven Webber <steven.webber@johnlewis.co.uk>
2023-02-13 17:53:12 +0200

    first fix

--- src/main/java/org/music/Main.java
+++ src/main/java/org/music/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String... args) throws Exception {
         String startPath;
         if (args.length < 1) {
-            System.out.println("Not directory specificed, using current directory");
+            System.out.println("No directory specified, using current directory");
             startPath = ".";
         }
         else {
